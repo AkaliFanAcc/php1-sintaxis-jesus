9ee450c874c20586db817e5785f9a6e435b525b7
AkaliFanAcc <jesus.gabriel.mn99@gmail.com>
2023-04-08 11:35:29 -0400

    added new files

--- exercise-18.php
+++ exercise-18.php
@@ -5,25 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Tipos de datos en PHP</title>
+    <title>Ejemplo 1 con la palabra reservada print</title>
 </head>
 <body>
     <?php
-        /* 
-            Las variables pueden almacenar datos de diferentes tipos
-            y diferentes tipos de datos pueden realizar cosas diferentes
-         */
-        //PHP admite los siguientes tipos de datos:
-        /* 
-            -string
-            -integer
-            -float
-            -booleana
-            -array
-            -object
-            -NULL
-            -Resource
-        */
+        print '<h2>PHP es divertido</h2>';
+        print 'Hola mundo<br>';
+        print "I'm living in Venezuela<br>";
+        print 'Ella me dijo "Buenos días" y yo le dije "Hola"<br>';
+        //Esta linea de código genera un error, ya que la palabra reservada 'print' sólo acepta 1 parámetro
+        //print "Esta ", "cadena de caracteres ", "puede ", "tener ", "multiples parametros";
     ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
