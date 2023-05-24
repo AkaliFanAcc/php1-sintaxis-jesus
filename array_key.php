c91f352e7707c7dd088828fd0093f619271a248a
Jesús Meléndez <jesus.gabriel.mn99@gmail.com>
2023-05-24 12:26:44 -0400

    Names files edited

--- array_key.php
+++ array_key.php
@@ -5,19 +5,12 @@
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ' crossorigin='anonymous'>
-  <title>sobreescritura variable local</title>
+  <title>Array tipo key</title>
 </head>
 <body>
   <?php
-    function miPrueba() {
-      static $x = 0;
-      echo "<br> $x";
-      $x++;
-    }
-
-    miPrueba();
-    miPrueba();
-    miPrueba();
+    $array = array("primero" => 0, "segundo" => 15, 3 => "hola", 4 => "mundo");
+    var_dump($array);
   ?>
 
   <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js' integrity='sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE' crossorigin='anonymous'></script>
